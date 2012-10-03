03eaa710cf1709aba9655b93f01e9773e1de1ed2
Igor <is.markelov@gmail.com>
2012-10-03 18:44:12 +0200

    done

--- 02-Homework 1. Quantum Harmonic Oscillator/runge_kutta_4th_steppin_method_rhs.m
+++ 02-Homework 1. Quantum Harmonic Oscillator/runge_kutta_4th_steppin_method_rhs.m
@@ -1,4 +1,5 @@
-function rhs=runge_kutta_4th_steppin_method_rhs(x,ic,dummy,beta)
+function rhs=runge_kutta_4th_steppin_method_rhs(x,ic,dummy,En)
 y1=ic(1);
 y2=ic(2);
-rhs=[y2;(beta-100)*y1];
+x;
+rhs=[y2;(x^2-En)*y1];
