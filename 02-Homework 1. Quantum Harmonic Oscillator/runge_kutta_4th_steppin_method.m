feb5d941630710ee1c17ebe9f72c365c6e2962a3
Igor <is.markelov@gmail.com>
2012-10-07 17:55:52 +0200

    Initioal conditions understanding

--- 02-Homework 1. Quantum Harmonic Oscillator/runge_kutta_4th_steppin_method.m
+++ 02-Homework 1. Quantum Harmonic Oscillator/runge_kutta_4th_steppin_method.m
@@ -4,15 +4,21 @@ tol=10^(-10);%tolerance
 xspan=[-4 4];%finding solution between 2 numbers of xspan
 
 A=1;
-ic=[0 A];%initial conditions
+ic=[0 A];%initial conditions y(0) y'(A)
 
-En=10;%betta
+En=16;% не получается что-то этот вывод...
+%Y''+betta*y=0,betta should be positive becouse 
+%we should have several answers there PSI=0 (from ic:PSI(-4,+4)=0) 
+%thats why sin and cos in answers not exp()
+%therefore -(x^2-En)>0 x(max)=+-4 >> 
 
 
 
-for jj=1:6% for finding more than 1 solution
+for jj=1:10% for finding more than 1 solution
 dEn=1;    
-    
+if En<0 
+    break
+end    
 for j=1:1000
 
 [t,y]=ode45('runge_kutta_4th_steppin_method_rhs',xspan,ic,[],En);
@@ -21,7 +27,8 @@ if abs(y(end,1))<tol
     [t y(:,1)];
     
     break
-end%     
+end% 
+
 if y(end,1)*((-1)^(jj+1))>0% last component of 1st column in power of jj+1, 
     %becouse after first solution the function become negative(like sin)
     %will be
@@ -32,6 +39,8 @@ dEn=dEn/2;%и меняем шаг
 end
 end
 
+
+    
 En
 En=En-1;
 y;
